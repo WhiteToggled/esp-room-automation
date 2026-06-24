b6e6596ded82772614a37f06184aa2ba8ab2a073
WhiteToggled <toggledwhite@gmail.com>
2026-06-24 16:37:36 +0500

    refactor : refactor firmware and add some qol

--- client_firmware/src/main.cpp
+++ client_firmware/src/main.cpp
@@ -1,96 +1,107 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
 
+#include "comms.h"
 #include "config.h"
-#include "wifi.h"
-#include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
+#include "soc/soc.h"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
+//  ----- VARIABLES -----------
 
-    String message = "";
-    for (int i = 0; i < length; i++) {
-        message += (char)payload[i];
+volatile bool switch_pressed[8] = {false};
+unsigned long switch_debounce_time[8] = {0};
+bool mqtt_pending_updates[8] = {false};
+bool mqtt_to_update[8] = {false};
+
+void IRAM_ATTR handleISR_0() { switch_pressed[0] = true; }
+void IRAM_ATTR handleISR_1() { switch_pressed[1] = true; }
+void IRAM_ATTR handleISR_2() { switch_pressed[2] = true; }
+void IRAM_ATTR handleISR_3() { switch_pressed[3] = true; }
+void IRAM_ATTR handleISR_4() { switch_pressed[4] = true; }
+void IRAM_ATTR handleISR_5() { switch_pressed[5] = true; }
+void IRAM_ATTR handleISR_6() { switch_pressed[6] = true; }
+void IRAM_ATTR handleISR_7() { switch_pressed[7] = true; }
+
+void (*ISR_functions[8])() = {handleISR_0, handleISR_1, handleISR_2,
+                              handleISR_3, handleISR_4, handleISR_5,
+                              handleISR_6, handleISR_7};
+
+void detect_switchboard();
+void sync_server();
+// ---------------------------
+
+void setup() {
+    Serial.begin(115200);
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout protect
+
+    pinMode(STATUS_LED, OUTPUT);
+    for (unsigned int i = 0; i < 8; ++i) {
+        pinMode(RELAY_PINS[i], OUTPUT);
+        digitalWrite(RELAY_PINS[i],
+                     HIGH); // activelow - turn off all relays during boot
+
+        pinMode(SWITCH_PINS[i], INPUT_PULLUP);
+        attachInterrupt(digitalPinToInterrupt(SWITCH_PINS[i]), ISR_functions[i],
+                        CHANGE);
     }
-    Serial.println(message);
 
-    int state = message.toInt();
-    state = !state;
-    if (strcmp(topic, TOPIC_INPUT_1) == 0) {
-        digitalWrite(PIN_INPUT_1, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_2) == 0) {
-        digitalWrite(PIN_INPUT_2, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_3) == 0) {
-        digitalWrite(PIN_INPUT_3, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_4) == 0) {
-        digitalWrite(PIN_INPUT_4, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_5) == 0) {
-        digitalWrite(PIN_INPUT_5, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_6) == 0) {
-        digitalWrite(PIN_INPUT_6, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_7) == 0) {
-        digitalWrite(PIN_INPUT_7, state);
-    } 
-    else if (strcmp(topic, TOPIC_INPUT_8) == 0) {
-        digitalWrite(PIN_INPUT_8, state);
-    } 
+    connect_to_wifi();
+
+    client.setServer(MQTT_SERVER, MQTT_PORT);
+    client.setCallback(callback);
 }
 
-void reconnect() {
-    // Loop until we're reconnected
-    while (!client.connected()) {
-        Serial.print("Attempting MQTT connection...");
-        // Attempt to connect with a unique client ID
-        if (client.connect("ESP32_Room1_Client")) {
-            Serial.println("connected");
-            // Subscribe to the room wildcard to catch all room 1 updates
-            client.subscribe("r1/#");
-        } else {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            delay(5000);
+void loop() {
+    if (WiFi.status() != WL_CONNECTED) {
+        wifi_reconnect();
+        return;
+    }
+
+    if (!client.connected()) {
+        mqtt_reconnect();
+        return;
+    }
+    client.loop();
+    detect_switchboard();
+    sync_server();
+}
+
+void detect_switchboard() {
+    unsigned long curr_time = millis();
+
+    for (int i = 0; i < 8; ++i) {
+        if (switch_pressed[i]) {
+            noInterrupts();
+            switch_pressed[i] = false;
+            interrupts();
+            if ((curr_time - switch_debounce_time[i]) > DEBOUNCE_DELAY) {
+                switch_debounce_time[i] = curr_time;
+
+                bool curr_state = digitalRead(SWITCH_PINS[i]);
+                // pullup so HIGH = OPEN, LOW = CLOSED
+
+                digitalWrite(RELAY_PINS[i], curr_state); // active-low relays
+                mqtt_pending_updates[i] = true;
+                mqtt_to_update[i] = !curr_state;
+                Serial.printf("Override: Switch [%s] %d\n", MQTT_TOPICS[i],
+                              !curr_state);
+            }
         }
     }
 }
 
+void sync_server() {
+    for (int i = 0; i < 8; ++i) {
+        if (mqtt_pending_updates[i]) {
+            mqtt_pending_updates[i] = false;
 
-void setup() {
-  Serial.begin(115200);
-
-  pinMode(PIN_INPUT_1, OUTPUT);
-  pinMode(PIN_INPUT_2, OUTPUT);
-  pinMode(PIN_INPUT_3, OUTPUT);
-  pinMode(PIN_INPUT_4, OUTPUT);
-  pinMode(PIN_INPUT_5, OUTPUT);
-  pinMode(PIN_INPUT_6, OUTPUT);
-  pinMode(PIN_INPUT_7, OUTPUT);
-  pinMode(PIN_INPUT_8, OUTPUT);
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  // disable brownout
-
-  pinMode(2, OUTPUT);
-
-  connect_to_wifi();
-
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  client.setCallback(callback);
-}
-
-void loop() {
-    if (!client.connected()) {
-        reconnect();
+            const char *payload = mqtt_to_update[i] ? "1" : "0";
+            client.publish(MQTT_TOPICS[i], payload);
+            Serial.printf("MQTT Publish: [%s] %s\n", MQTT_TOPICS[i], payload);
+        }
     }
-    client.loop();
 }
