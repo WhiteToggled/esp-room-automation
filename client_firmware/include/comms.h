b4806d8413e3bbe4dd928cccc67f99a2b197ecff
WhiteToggled <toggledwhite@gmail.com>
2026-07-06 21:35:05 +0500

    feat: firmware update add ota updates connect to mqtt secured (8883) other qol features

--- client_firmware/include/comms.h
+++ client_firmware/include/comms.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <WiFiClientSecure.h>
 
 void connect_to_wifi();
 void wifi_reconnect();
@@ -6,7 +7,20 @@ void wifi_reconnect();
 void callback(char* topic, byte* payload, unsigned int length);
 void mqtt_reconnect();
 
-extern char mqtt_server[40];
+extern char mqtt_username[64];
+extern char mqtt_pass[32];
+extern char mqtt_server[128];
 extern char mqtt_port[6];
-extern char mqtt_client_id[40];
+extern char mqtt_client_id[64];
+extern bool update_config;
+
+extern WiFiClientSecure espClient;
+extern PubSubClient client;
+extern WiFiManager wm;
 extern Preferences prefs;
+
+extern WiFiManagerParameter param_mqtt_user;
+extern WiFiManagerParameter param_mqtt_pass;
+extern WiFiManagerParameter param_mqtt_server;
+extern WiFiManagerParameter param_mqtt_port;
+extern WiFiManagerParameter param_mqtt_id;
--- client_firmware/src/comms.cpp
+++ client_firmware/src/comms.cpp
@@ -1,43 +1,59 @@
+#include <HTTPUpdate.h>
 #include <Preferences.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <WiFiManager.h>
 
 #include "comms.h"
 #include "config.h"
 
-extern PubSubClient client;
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+WiFiManager wm;
 Preferences prefs;
 
-char mqtt_server[40] = "";
-char mqtt_port[6] = "1883";
-char mqtt_client_id[40] = "ESP32_Nestboard";
+char mqtt_username[64] = "test";
+char mqtt_pass[32] = "testtest";
+char mqtt_server[128] = "8d05a36cd6004e98b1164a0f2d05cbba.s1.eu.hivemq.cloud";
+char mqtt_port[6] = "8883";
+char mqtt_client_id[64] = "ESP32_Nestboard";
 bool update_config = false;
+
+WiFiManagerParameter param_mqtt_user("username", "MQTT Username", mqtt_username,
+                                     64);
+WiFiManagerParameter param_mqtt_pass("password", "MQTT Password", mqtt_pass,
+                                     32);
+WiFiManagerParameter param_mqtt_server("server", "MQTT Server IP", mqtt_server,
+                                       128);
+WiFiManagerParameter param_mqtt_port("port", "MQTT Port", mqtt_port, 6);
+WiFiManagerParameter param_mqtt_id("client_id", "MQTT Client ID",
+                                   mqtt_client_id, 64);
 
 void update_cfg_callback() { update_config = true; }
 
 void connect_to_wifi() {
     prefs.begin("nestboard-cfg", false);
 
-    String pref_server = prefs.getString("mq_server", "");
-    String pref_port = prefs.getString("mq_port", "1883");
+    String pref_username = prefs.getString("mq_user", "test");
+    String pref_pass = prefs.getString("mq_pass", "testtest");
+    String pref_server = prefs.getString(
+        "mq_server", "8d05a36cd6004e98b1164a0f2d05cbba.s1.eu.hivemq.cloud");
+    String pref_port = prefs.getString("mq_port", "8883");
     String pref_client_id = prefs.getString("mq_id", "ESP32_Nestboard");
 
+    strcpy(mqtt_username, pref_username.c_str());
+    strcpy(mqtt_pass, pref_pass.c_str());
     strcpy(mqtt_server, pref_server.c_str());
     strcpy(mqtt_port, pref_port.c_str());
     strcpy(mqtt_client_id, pref_client_id.c_str());
 
-    WiFiManager wm;
     wm.setConfigPortalBlocking(false);
     wm.setSaveConfigCallback(update_cfg_callback);
     wm.setConnectTimeout(10);
 
-    WiFiManagerParameter param_mqtt_server("server", "MQTT Server IP",
-                                           mqtt_server, 40);
-    WiFiManagerParameter param_mqtt_port("port", "MQTT Port", mqtt_port, 6);
-    WiFiManagerParameter param_mqtt_id("client_id", "MQTT Client ID",
-                                       mqtt_client_id, 40);
-
+    wm.addParameter(&param_mqtt_user);
+    wm.addParameter(&param_mqtt_pass);
     wm.addParameter(&param_mqtt_server);
     wm.addParameter(&param_mqtt_port);
     wm.addParameter(&param_mqtt_id);
@@ -46,11 +62,10 @@ void connect_to_wifi() {
     digitalWrite(STATUS_LED, HIGH);
 
     if (!wm.autoConnect("Nestboard-Setup")) {
-        Serial.println( "Config Portal timeout or failure. Starting offline ...");
+        Serial.println(
+            "Config Portal timeout or failure. Starting offline ...");
         digitalWrite(STATUS_LED, LOW);
         return;
-        // delay(3000);
-        // ESP.restart();
     }
 
     digitalWrite(STATUS_LED, LOW);
@@ -58,10 +73,14 @@ void connect_to_wifi() {
     Serial.println(WiFi.localIP());
 
     if (update_config) {
+        strcpy(mqtt_username, param_mqtt_user.getValue());
+        strcpy(mqtt_pass, param_mqtt_pass.getValue());
         strcpy(mqtt_server, param_mqtt_server.getValue());
         strcpy(mqtt_port, param_mqtt_port.getValue());
         strcpy(mqtt_client_id, param_mqtt_id.getValue());
 
+        prefs.putString("mq_user", String(mqtt_username));
+        prefs.putString("mq_pass", String(mqtt_pass));
         prefs.putString("mq_server", String(mqtt_server));
         prefs.putString("mq_port", String(mqtt_port));
         prefs.putString("mq_id", String(mqtt_client_id));
@@ -90,11 +109,13 @@ void mqtt_reconnect() {
     lastAttempt = now;
 
     Serial.print("MQTT connection lost, reconnecting...");
-    if (client.connect(mqtt_client_id)) {
+
+    if (client.connect(mqtt_client_id, mqtt_username, mqtt_pass)) {
         Serial.println("MQTT Connected!");
         for (int i = 0; i < N_DEVICES; i++) {
             client.subscribe(MQTT_TOPICS[i]);
         }
+        client.subscribe(FIRMWARE_UPDATE_TOPIC);
     } else {
         Serial.printf("MQTT Failed, rc=%d. Try again in 5 seconds.\n",
                       client.state());
@@ -102,6 +123,31 @@ void mqtt_reconnect() {
 }
 
 // -------------- MQTT --------------
+void update_ota(const char* url) {
+    Serial.printf("Starting OTA update from URL: %s\n", url);
+
+    client.disconnect();
+
+    WiFiClientSecure updateClient;
+    updateClient.setInsecure();
+    t_httpUpdate_return ret = httpUpdate.update(updateClient, url);
+    switch (ret) {
+    case HTTP_UPDATE_FAILED:
+        Serial.printf("OTA Failed (%d): %s\n", httpUpdate.getLastError(),
+                      httpUpdate.getLastErrorString().c_str());
+        break;
+    case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("No new updates available.");
+        break;
+    case HTTP_UPDATE_OK:
+        Serial.println("OTA Successful! Rebooting...");
+        // The ESP32 usually restarts automatically on success, but just in
+        // case:
+        ESP.restart();
+        break;
+    }
+    return;
+}
 
 void callback(char *topic, byte *payload, unsigned int length) {
     char message[256];
@@ -110,6 +156,11 @@ void callback(char *topic, byte *payload, unsigned int length) {
     memcpy(message, payload, length);
     message[length] = '\0';
     Serial.printf("Message arrived: [%s] %s\n", topic, message);
+
+    if (strcmp(topic, FIRMWARE_UPDATE_TOPIC) == 0) {
+        update_ota(message);
+        return;
+    }
 
     int target_state = atoi(message);
     target_state = !target_state;
